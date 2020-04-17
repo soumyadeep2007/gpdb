23106f6cf194b55f9cc142b36459b0bd1cb2fc18
Jesse Zhang <sbjesse@gmail.com>
2020-04-17 16:24:48 -0700

    Fix assert condition in spill_hash_table()
    This commit fixes the following assertion failure message reported in:
    (#9902) https://github.com/greenplum-db/gpdb/issues/9902
    
    FailedAssertion("!(hashtable->nbuckets > spill_set->num_spill_files)", File: "execHHashagg.c", Line: 1355)
    
    hashtable->nbuckets can actually end up being equal to
    spill_set->num_spill_files, which causes the failure. This is because:
    
    hashtable->nbuckets is set with HashAggTableSizes->nbuckets, which can
    end up being equal to: gp_hashagg_default_nbatches. Refer:
    nbuckets = Max(nbuckets, gp_hashagg_default_nbatches);
    
    Also, spill_set->num_spill_files is set with
    HashAggTableSizes->nbatches, which is further set to
    gp_hashagg_default_nbatches.
    
    Thus, these two entities can be equal.
    
    Co-authored-by: Soumyadeep Chakraborty <sochakraborty@pivotal.io>
    (cherry picked from commit 067bb35059948e83cadae8fd3b0f14e6fee9d399)

--- src/backend/executor/execHHashagg.c
+++ src/backend/executor/execHHashagg.c
@@ -1359,7 +1359,7 @@ spill_hash_table(AggState *aggstate)
 	/* Book keeping. */
 	hashtable->is_spilling = true;
 
-	Assert(hashtable->nbuckets > spill_set->num_spill_files);
+	Assert(hashtable->nbuckets >= spill_set->num_spill_files);
 
 	/*
 	 * Write each spill file. Write the last spill file first, since it will
